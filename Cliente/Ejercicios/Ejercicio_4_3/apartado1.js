6db07f92d7d573d97c5be9a03187f908edb53e9b
mafese3 <marinaserranof@gmail.com>
2026-03-04 10:51:14 +0100

    Cliente: Started JavaScript exercises

---
+++ Cliente/Ejercicios/Ejercicio_4_3/apartado1.js
@@ -0,0 +1,24 @@
+// ------------------------------Ejercicio 1----------------------------------------
+
+let elemento = 5
+console.log("Valor: ", elemento, " | Tipo: ", typeof(elemento) )
+elemento = "Hola por consola"
+console.log("Valor: ", elemento, " | Tipo: ", typeof(elemento) )
+elemento = true
+console.log("Valor: ", elemento, " | Tipo: ", typeof(elemento) )
+elemento = null
+console.log("Valor: ", elemento, " | Tipo: ", typeof(elemento) )
+
+
+// ------------------------------Ejercicio 2----------------------------------------
+
+const num = 3.14159
+const numString = num.toString()
+const numDec = num.toFixed(2)
+const numExp = num.toExponential()
+
+console.log("Valor: ", num, " | Tipo: ", typeof(num) )
+console.log("Valor: ", numString, " | Tipo: ", typeof(numString) )
+console.log("Valor: ", numDec, " | Tipo: ", typeof(numDec) )
+console.log("Valor: ", numExp, " | Tipo: ", typeof(numExp) )
+
