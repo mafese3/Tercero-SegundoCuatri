897a7766630964b76e56f7e2a76ff6dba58823f8
Marina Fernandez <marinaserranof@gmail.com>
2026-03-09 17:34:00 +0100

    Cliente: Exercises JavaScript 2

--- Cliente/Ejercicios/JS/Ejercicio_5_3/ejercicio1/ejercicio1.js
+++ Cliente/Ejercicios/JS/Ejercicio_5_3/ejercicio1/ejercicio1.js
@@ -1,7 +1,25 @@
 const boton = document.querySelector("button")
 
+function crearTabla() {
+    if(document.querySelector("table") == null) {
+        let tabla = document.createElement("table")
+
+        for (let i = 0 ; i < 2; i++) {
+            let fila = document.createElement("tr")
+            for (let j = 0; j < 2; j++) {
+                let texto = document.createTextNode(`Celda: ${i}, ${j}`)
+                let celda = document.createElement("td")
+                celda.appendChild(texto)
+                fila.appendChild(celda)
+            }
+            tabla.appendChild(fila)
+        }
+        tabla.style.border = "2px solid blue"
+        document.body.appendChild(tabla)
+    } else {
+        alert("Ya has creado una tabla.")
+    }
+}
 
 
-
-
-document.addEventListener("click", crearTabla)
+boton.addEventListener("click", crearTabla)
