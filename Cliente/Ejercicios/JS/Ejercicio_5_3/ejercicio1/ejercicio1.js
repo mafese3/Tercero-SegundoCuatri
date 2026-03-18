9aebbac8ba6656a5dda3b8a02b00941fc283ca02
mafese3 <marinaserranof@gmail.com>
2026-03-18 19:17:16 +0100

    Cliente: Acabada la última relación de ejercicios de CSS

--- Cliente/Ejercicios/JS/Ejercicio_5_3/ejercicio1/ejercicio1.js
+++ Cliente/Ejercicios/JS/Ejercicio_5_3/ejercicio1/ejercicio1.js
@@ -1,14 +1,27 @@
 const boton = document.querySelector("button")
 
-function crearTabla() {
-    if(document.querySelector("table") == null) {
-        let tabla = document.createElement("table")
 
-        for (let i = 0 ; i < 2; i++) {
+function crearTabla() {
+    tablaExistente = document.querySelector("table")
+    if (tablaExistente){
+        tablaExistente.remove()
+    } else {
+        document.querySelector("img").remove()
+    }
+    
+    let tabla = document.createElement("table")
+    const rows = document.querySelector("#filas").value
+    const columns = document.querySelector("#columnas").value
+
+    //Controla que solo se cree tabla cuando los valores introducidos son menores que 7.
+    if(rows <= 7 && rows >= 1 && columns <= 7 && rows >= 1){
+        
+        for (let i = 0 ; i < rows; i++) {
             let fila = document.createElement("tr")
-            for (let j = 0; j < 2; j++) {
+            for (let j = 0; j < columns; j++) {
                 let texto = document.createTextNode(`Celda: ${i}, ${j}`)
                 let celda = document.createElement("td")
+                celda.style.padding = "0.75rem"
                 celda.appendChild(texto)
                 fila.appendChild(celda)
             }
@@ -16,9 +29,18 @@ function crearTabla() {
         }
         tabla.style.border = "2px solid blue"
         document.body.appendChild(tabla)
+    
+    //Un pequeño easter egg
+    } else if(rows == 67 || columns == 67){
+        let imagen = document.createElement("img")
+        imagen.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5Ik_Lh13wz3eLI3ClnfNpxjyW4Kdv3nyUUA&s"
+        document.body.appendChild(imagen)
     } else {
-        alert("Ya has creado una tabla.")
+        let imagen = document.createElement("img")
+        imagen.src = "https://i.pinimg.com/236x/d0/7b/6d/d07b6d2276a14cb61df6c4c9d0091caa.jpg"
+        document.body.appendChild(imagen)
     }
+    
 }
 
 
