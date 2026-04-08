651021294834c5213b20d635b2c840a7c0075664
mafese3 <marinaserranof@gmail.com>
2026-04-08 11:10:49 +0200

    General: Arreglado un problema de enlace de las carpetas

--- Servidor/Ejercicios/Ejercicio18_3/moviesTESAW/src/main/java/es/tesaw/movies/dao/MoviesRepository.java
+++
@@ -1,7 +0,0 @@
-package es.tesaw.movies.dao;
-
-import es.tesaw.movies.entity.MovieEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MoviesRepository extends JpaRepository<MovieEntity, Integer> {
-}
--- Servidor/Ejercicios/Ejercicio2_3/moviesTESAW/src/main/java/es/tesaw/movies/dao/MoviesRepository.java
+++
@@ -1,8 +0,0 @@
-package es.tesaw.movies.dao;
-
-import es.tesaw.movies.entity.Movies;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MoviesRepository extends JpaRepository<Movies, Long>{
-    
-}
